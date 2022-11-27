056484ad2902d534d813650e654cb01ecc8254a8
OUSMOI Anass <anass.ousmoi@soprasteria.com>
2022-11-27 22:59:21 +0100

    add log4j

--- src/main/java/td/BanqueApp.java
+++ src/main/java/td/BanqueApp.java
@@ -2,6 +2,8 @@ package td;
 
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 import td.dao.CompteDao;
 import td.dao.CompteDaoFichier;
 import td.dao.UserDao;
@@ -11,33 +13,33 @@ import td.service.UserService;
 
 public class BanqueApp {
 
+	final static Logger logger = Logger.getLogger(BanqueApp.class);
+	
 	public static void main(String[] args) {
-		
 
 		
-
 		try {
-			
+
 			Scanner scanner = new Scanner(System.in);
 			UserDao userDao = new UserDao();
-			
 			CompteDao compteDao = new CompteDao();
-			//CompteDaoFichier compteDaoFichier = new CompteDaoFichier("stockage.txt");
+			// CompteDaoFichier compteDaoFichier = new CompteDaoFichier("stockage.txt");
 
+			
 			UserService userService = new UserService(userDao);
 			CompteService compteService = new CompteService(compteDao);
-			
-			Menu menu = new Menu(scanner,userService,compteService);
-			
+
+			logger.info("Lancement Application");
+			Menu menu = new Menu(scanner, userService, compteService);
+
 			menu.afficher();
 			
-		
+			logger.info("Fin Application");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	
-	
+
 	}
 
 }
